88042deb8206e19a03d96b7f68b432f867836c24
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-30 16:13:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,10 @@
 def first_challenge
    epic_tragedy = {
      montague: {
-       patriarch:{} , 
-       matriarch:{} , 
-       hero:{} , 
-       hero_friends:[] }
+       patriarch:{name: "Lord Montague", age: "53" } , 
+       matriarch:{name: "Lady Montague", age: "54"} , 
+       hero:{name: "Romeo", age: "54"} , 
+       hero_friends:[{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}] }
      
      capulet: {
        patriarch:{} , 
